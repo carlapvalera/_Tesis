33cfd6d1bd5ff28d41b911ae96f71e0dc8dabd72
carlapvalera <perezvaleracarlas@gmail.com>
2024-12-03 13:20:12 -0500

    get function and run this  test

--- this/llm.py
+++ this/llm.py
@@ -2,15 +2,6 @@ import openai
 from pydantic import BaseModel, Field
 from fireworks.client import Fireworks
 
-class LLM():
-    def __init__(self, api_key: str = "fw_3ZXb6WWaLuUNnJZnvXWTwEY2", model: str = "accounts/fireworks/models/llama-v3p1-8b-instruct") -> None:
-        self.__SEND_GLOBAL_STATEMENT = True
-        self.__api_key = api_key
-        self.__model = model
-        self.__global_statements = []
-
-        self.__client = Fireworks(api_key=self.__api_key)
-
 client = openai.OpenAI(
     base_url="https://api.fireworks.ai/inference/v1",
     api_key="fw_3Zf7HrTqgxNgJFhWovNoxZcZ",
