6fcc78e7cb393bed25f0f4413a6490cbe0ca3641
carlapvalera <perezvaleracarlas@gmail.com>
2024-12-18 16:29:53 -0500

    arreglando cid

--- onei_model/extract_6.py
+++ onei_model/extract_6.py
@@ -1,14 +1,24 @@
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 
+
 lang = {
     3:   " ",
     4:   "!",
     11:  "(",
     12:  ")",
     97:  "~",
+    105: "á",
     108: "ä",
+    116: "í",
+    
+    121: "ó",
+    123: "ú",
     129: "ü",
+    203: "Í",
+    207 : "Ó",
+
+
 }
 ind = 68
 for cha in "abcdefghijklmnopqrstuvwxyz":
@@ -22,15 +32,12 @@ ind = 3
 for cha in " !\"#$%&'()*+,-./0123456789:":
     lang[ind] = cha
     ind += 1
-
-
 def to_acctual_characters(text: str):
     ret = ""
     for number in text.replace("(cid:", ";").replace(")", "")[1:].split(";"):
         # Verificar si cleaned_number es un número
         if not number.replace("\n", "").isdigit():
             continue  # Si no es un número, continuar con la 
-
         if int(number.replace("\n", "")) in lang:
             ret = ret + lang[int(number.replace("\n", ""))]
         else:
@@ -38,8 +45,6 @@ def to_acctual_characters(text: str):
         if "\n" in number:
             ret = ret + "\n"
     return ret
-
-
 for page_layout in extract_pages("C:\\blabla\\_Tesis\\01-territorio.pdf"):
     for element in page_layout:
         if isinstance(element, LTTextContainer):
