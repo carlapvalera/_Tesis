a3610b0540fca0b006ad2d6ab89e61974b959bda
carlapvalera <perezvaleracarlas@gmail.com>
2024-12-29 03:56:30 -0500

    my gemini api contador de roken y generacion de embeddings

---
+++ new/gemini_api.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+class Gemini_API:
+    def __init__(self):
+        # Cargar las variables de entorno desde el archivo .env
+        load_dotenv()
+        self.api_key = os.getenv("API_KEY")
+        # Configura Google Generative AI con la clave API
+        genai.configure(api_key=self.api_key)
+        self.model = genai.GenerativeModel("gemini-pro")
+
+    def count_tokens(self, text:str):
+        """Cuenta los tokens en el texto utilizando la API de Google Generative AI."""
+        return self.model.count_tokens(text)
+
+    def get_embeddings_query(self,query:str):
+        "RETRIEVAL_QUERY"
+        """Método para obtener embeddings .Especifica que el texto dado es una consulta en un parámetro de configuración de búsqueda/recuperación."""
+        result = genai.embed_content(
+        model="models/embedding-001",
+        content=query,
+        task_type="retrieval_document")
+
+        # 1 input > 1 vector output
+        print(str(result['embedding'])[:50], '... TRIMMED]')
+        return result
+
+    def get_embeddings_list(self,lista:list[str]):
+        "RETRIEVAL_QUERY"
+        "Especifica que el texto dado es una consulta en un parámetro de configuración de búsqueda/recuperación."
+        
+        result = genai.embed_content(
+        model="models/embedding-001",
+        content=lista,
+        task_type="retrieval_document")
+
+        # A list of inputs > A list of vectors output
+        for v in result['embedding']:
+            print(str(v)[:50], '... TRIMMED ...')
+        return result
+                
+
+   
