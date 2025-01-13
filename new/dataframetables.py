6b6a8a6c2771bb2df344a131963602ae9a242f5e
carlapvalera <perezvaleracarlas@gmail.com>
2025-01-13 01:29:25 -0500

    borar ejemplo d uso

--- new/dataframetables.py
+++ new/dataframetables.py
@@ -52,18 +52,3 @@ def get_table(self, text: str):
 
         return name, headers, data_table
 
-# Ejemplo de uso
-if __name__ == "__main__":
-    creator = DataFrameCreator()
-
-    # Simulando un texto de entrada para extraer una tabla
-    input_text = "1.1 - Situación geográfica de Cuba  CONCEPTO Lugar Provincias Latitud Norte Greenwich Archipiélago Cubano    Extremo septentrional Cayo Cruz del Padre Matanzas 23º16' 80º55'    Extremo meridional Punta del Inglés Granma 19º49' 77º40'    Extremo oriental Punta de Maisí Guantánamo 20º13' 74º08'    Extremo occidental Cabo de San Antonio Pinar del Río 21º52' 84º57'  Isla de Cuba (a)    Extremo septentrional Punta Hicacos Matanzas 23º11' 81º09'  Isla de la Juventud    Extremo septentrional Punta de Tirry - 21º57' 82º58'    Extremo meridional Caleta de Agustín Jol - 21º26' 82º54'    Extremo oriental Punta del Este - 21º34' 82º33'    Extremo occidental Punta Francés - 21º38' 83º11' (a) Los demás puntos extremos de la Isla de Cuba son los mismos señalados para la totalidad del archipiélago. Fuente: Síntesis Geográfica, Económica y Cultural de Cuba, versión digital, año 2017 y mapa plegable, Cuba. División                Político - Administrativa, año 2011. "
-    
-    # Llamar al método get_table para obtener la tabla
-    name, headers, df = creator.get_table(input_text)
-
-    # Mostrar los resultados
-    print("Nombre de la Tabla:", name)
-    print("Encabezados:", headers)
-    print("DataFrame:")
-    print(df)
